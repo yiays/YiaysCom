6187951e711c67cb6e4191ac3e167c848009777c
Yiays <yesiateyoursheep@gmail.com>
2023-01-15 00:10:57 +1300

    Optimised post fetching

--- includes/blogdata.php
+++ includes/blogdata.php
@@ -142,36 +142,84 @@ class Article {
 class Author {
   public int $id;
   public string $username;
-  public ?string $pfp;
-  public ?string $bio;
 
   function setup($row)
   {
     $this->id = $row['UserID'];
     $this->username = $row['Username'];
-    $this->pfp = null;
-    $this->bio = null;
   }
 
   function handle() : string{
     return $this->username;
   }
 }
+class Comment {
+  public int $id;
+  public Author $author;
+  public string $content;
+  public ?Comment $reply;
+  public DateTime $date;
+  public bool $hidden;
 
-$articles = [];
-$result = $conn->query(
-  "SELECT post.PostID,Title,Url,Tags,Cover,Colour,Date,EditDate,Content,Hidden,post.UserID,passport.user.Username,COUNT(viewers.ViewTime) AS Views
-  FROM (post
-    LEFT JOIN passport.user ON post.UserID = passport.user.Id)
-    LEFT JOIN viewers ON post.PostID = viewers.PostID
-  GROUP BY post.PostID
-  ORDER BY Date DESC"
-);
-if(!$result) {
-  throw new Exception($conn->error);
+  function setup($row) {
+    $this->id = $row['CommentID'];
+    $this->author = new Author();
+    $this->author->setup($row);
+    $this->content = $row['Content'];
+    $this->reply = null;
+    $this->date = new DateTime($row['PublishDate']);
+    $this->hidden = $row['Hidden'];
+  }
 }
-while($row = $result->fetch_assoc()){
-  $article = new Article();
-  $article->setup($row);
-  $articles[] = $article;
+
+function fetch_articles($trim=false, $start=0, $limit=20): array {
+  global $conn;
+
+  $contentq = ($trim?"SUBSTRING(Content, 1, $trim) AS Content":'Content');
+  $result = $conn->query(
+    "SELECT post.PostID,Title,Url,Tags,Cover,Colour,Date,EditDate,$contentq,Hidden,post.UserID,passport.user.Username,COUNT(viewers.ViewTime) AS Views
+    FROM (post
+      LEFT JOIN passport.user ON post.UserID = passport.user.Id)
+      LEFT JOIN viewers ON post.PostID = viewers.PostID
+    GROUP BY post.PostID
+    ORDER BY Date DESC
+    LIMIT $start, $limit"
+  );
+  if(!$result) {
+    throw new Exception($conn->error);
+  }
+  $articles = [];
+  while($row = $result->fetch_assoc()){
+    $article = new Article();
+    $article->setup($row);
+    $articles[] = $article;
+  }
+
+  return $articles;
+}
+
+function fetch_article($url=null, $id=null): ?Article {
+  global $conn;
+
+  if(!is_null($id)) $id = (int)$id;
+  $result = $conn->query(
+    "SELECT post.PostID,Title,Url,Tags,Cover,Colour,Date,EditDate,Content,Hidden,post.UserID,passport.user.Username,COUNT(viewers.ViewTime) AS Views
+    FROM (post
+      LEFT JOIN passport.user ON post.UserID = passport.user.Id)
+      LEFT JOIN viewers ON post.PostID = viewers.PostID
+    WHERE ".($url?"Url = '".$conn->real_escape_string($url)."'":"PostID = $id")."
+    GROUP BY post.PostID
+    ORDER BY Date DESC"
+  );
+  if(!$result) {
+    throw new Exception($conn->error);
+  }
+  if($result->num_rows == 1) {
+    $article = new Article();
+    $article->setup($result->fetch_assoc());
+
+    return $article;
+  } else {
+    return null;
+  }
 }
