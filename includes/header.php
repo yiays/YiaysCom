6e8a839e9a0a3102dde40e25019194d6432bb2f6
Yiays <yesiateyoursheep@gmail.com>
2023-06-09 16:10:11 +1200

    Removed old version of site and cleaned up

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,7 @@
     <meta name="og:url" content="<?php echo "https://yiays.com".$url; ?>">
     <meta name="twitter:card" content="summary_large_image">
 
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
@@ -28,56 +29,8 @@
 		
 		<link rel="stylesheet" href="/normalize.css">
 		<link rel="stylesheet" href="/style.css?v=0.3.5">
-
-    <style>
-      .beta-toggle {
-        position: absolute;
-        top: 1.5em;
-        right: 1.5em;
-        display: inline-block;
-        padding: 0.4em;
-        background: #000;
-        color: #fff;
-        box-shadow: #000 0 0 0.1em;
-        border-radius: 1em;
-        z-index: 1;
-      }
-      .beta-toggle > .beta-slider {
-        position: relative;
-        display: inline-block;
-        width: 1.8em;
-        height: 1em;
-        top: 0.1em;
-        margin-right: 0.2em;
-				background: rgb(163,73,164);
-        border-radius: 1em;
-        transition: background 0.25s ease;
-      }
-      .beta-toggle > .beta-slider::before {
-        content: '';
-        position: absolute;
-        left: 0.1em;
-        top: 0.1em;
-        display: inline-block;
-        width: 0.8em;
-        height: 0.8em;
-        background: #fff;
-        border-radius: 0.8em;
-        transition: left 0.25s ease;
-      }
-      .beta-toggle:focus > .beta-slider {
-        background: rgb(163,73,164);
-      }
-      .beta-toggle:focus > .beta-slider::before {
-        left: 0.1em;
-      }
-    </style>
   </head>
   <body>
-    <a class="beta-toggle" href="/?old=1">
-      <span class="beta-slider"></span>
-      Legacy
-    </a>
     <div class="main">
       <nav>
         <p class="nav-item show-small text-center text-skinny" style="width:100%;">
--- router.php
+++ router.php
@@ -1,38 +1,23 @@
 <?php
-if(isset($_GET['old'])) {
-  setcookie('old', $_GET['old'], time()+60*60*24*30, '/', 'yiays.com', true, false);
-  http_response_code(401);
-  header('location: /');
-  die();
-}
-
 $url = explode('?', $_SERVER['REQUEST_URI'])[0];
 
-if(key_exists('old', $_COOKIE) && $_COOKIE['old']) {
-  if(strlen($url) > 1) {
-    http_response_code(404);
-    die();
-  }
-  require('old.html');
-}else{
-  if($url == '/'){
-    require('index.php');
-  }
-  elseif(str_starts_with($url, '/projects/')) {
-    require('projects.php');
-  }
-  elseif(str_starts_with($url, '/blog/')) {
-    require('blog/index.php');
-  }
-  elseif(str_starts_with($url, '/services/')) {
-    require('services.php');
-  }
-  /*elseif(str_starts_with($url, '/login/')) {
-    require('auth.php');
-  }*/
-  else {
-    http_response_code(404);
-    die();
-  }
+if($url == '/'){
+  require('index.php');
+}
+elseif(str_starts_with($url, '/projects/')) {
+  require('projects.php');
+}
+elseif(str_starts_with($url, '/blog/')) {
+  require('blog/index.php');
+}
+elseif(str_starts_with($url, '/services/')) {
+  require('services.php');
+}
+/*elseif(str_starts_with($url, '/login/')) {
+  require('auth.php');
+}*/
+else {
+  http_response_code(404);
+  die();
 }
 ?>
